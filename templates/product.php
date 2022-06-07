5d0ff882e5906b260d2a691933dbdd53b6af19bd
longvt93 <vutuanlongdhcn@gmail.com>
2022-06-07 17:33:37 +0700

    Tạo form popup confirm khi delete sản phẩm

--- templates/product.php
+++ templates/product.php
@@ -8,7 +8,7 @@
 		<h2>Danh sách sản phẩm</h2>
 		<button class="btn btn_add_product">Thêm sản phẩm</button>
 	</div> -->
-	<div class="crm-content">
+	<div class="crm-content" x-data="data()">
 		<div class="crm-table">
 			<h2 class="mt-4 mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Danh sách sản phẩm</h2>
 			<table class="table table-striped w-full overflow-hidden rounded-lg shadow-xs">
@@ -47,7 +47,7 @@
 											<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
 										</svg>
 									</button>
-									<button class="button-remove flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+									<button data-product="<?= esc_attr( $product->id ) ?>" @click="openModal" class="button-remove flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
 										<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
 											<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
 										</svg>
@@ -56,6 +56,80 @@
 							</td>
 						</tr>
 					<?php endforeach; ?>
+					<div
+						x-show="isModalOpen"
+						x-transition:enter="transition ease-out duration-150"
+						x-transition:enter-start="opacity-0"
+						x-transition:enter-end="opacity-100"
+						x-transition:leave="transition ease-in duration-150"
+						x-transition:leave-start="opacity-100"
+						x-transition:leave-end="opacity-0"
+						class="fixed inset-0 z-30 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"
+					>
+						<!-- Modal -->
+						<div
+							x-show="isModalOpen"
+							x-transition:enter="transition ease-out duration-150"
+							x-transition:enter-start="opacity-0 transform translate-y-1/2"
+							x-transition:enter-end="opacity-100"
+							x-transition:leave="transition ease-in duration-150"
+							x-transition:leave-start="opacity-100"
+							x-transition:leave-end="opacity-0  transform translate-y-1/2"
+							@click.away="closeModal"
+							@keydown.escape="closeModal"
+							class="form-popup-remove w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-gray-800 sm:rounded-lg sm:m-4 sm:max-w-xl"
+							id="modal"
+						>
+							<!-- Remove header if you don't want a close icon. Use modal body to place modal tile. -->
+							<header class="flex justify-end">
+								<button
+								class="inline-flex items-center justify-center w-6 h-6 text-gray-400 transition-colors duration-150 rounded dark:hover:text-gray-200 hover: hover:text-gray-700"
+								aria-label="close"
+								@click="closeModal"
+								>
+								<svg
+									class="w-4 h-4"
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									role="img"
+									aria-hidden="true"
+								>
+									<path
+									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+									clip-rule="evenodd"
+									fill-rule="evenodd"
+									></path>
+								</svg>
+								</button>
+							</header>
+							<!-- Modal body -->
+							<div class="mt-4 mb-6">
+								<!-- Modal title -->
+								<p class="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-300">Xác nhận</p>
+								<!-- Modal description -->
+								<p class="text-sm text-gray-700 dark:text-gray-400">
+									Bạn có chắc chắn muốn xóa sản phẩm này
+								</p>
+							</div>
+							<footer
+								class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800"
+							>
+								<button
+								@click="closeModal"
+								class="w-full px-5 py-3 text-sm font-medium leading-5 text-white text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 sm:px-4 sm:py-2 sm:w-auto active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
+								>
+								Hủy
+								</button>
+								<button
+								:class="isModalOpen ? 'confirmed' : ''"
+								@click="closeModal"
+								class="confirm-remove w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+								>
+								Xác nhận
+								</button>
+							</footer>
+						</div>
+					</div>
 				</tbody>
 			</table>
 		</div>
