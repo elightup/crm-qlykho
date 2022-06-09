2f92ce1ff1e92e86964bb87f0aefcd0ae5100c56
tuyenlq94 <tuyenicolor@gmail.com>
2022-06-09 17:39:45 +0700

    fix htmlLayout khi add sản phẩm kho

--- assets/js/kho.js
+++ assets/js/kho.js
@@ -176,22 +176,23 @@ jQuery( function ( $ ) {
 			product_kho.removeButton();
 		},
 		htmlLayout: function ( data ) {
+			//console.log( 'data', data );
 			products = data.products;
 			inner = ``;
 			$.each( products, function ( i, val ) {
 				inner = inner + `
-				<div class="modal-body__inner">
+				<div class="modal-body__inner" data-product="${ products[ i ][ 'id_product' ] }">
 					<div class="modal-body__id px-4 py-3">#${ products[ i ][ 'id_product' ] }</div>
 					<div data-name="${ products[ i ][ 'name_sp' ] }" class="product__name px-4 py-3">${ products[ i ][ 'name_sp' ] }</div>
 					<div data-number="${ products[ i ][ 'number' ] }" class="product__number px-4 py-3">${ products[ i ][ 'number' ] }</div>
 					<div class="modal-body__action px-4 py-3">
 						<div class="flex items-center space-x-4 text-sm">
-							<button class="button-edit flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
+							<button data-kho="${ data.id_kho }" class="edit-product flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
 								<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
 									<path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
 								</svg>
 							</button>
-							<button data-kho="<?= esc_attr( $warehouse->id ) ?>" @click="openList" class="button-remove flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
+							<button data-kho="${ data.id_kho }" @click="openList" class="remove-product flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
 								<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
 									<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
 								</svg>
@@ -220,7 +221,6 @@ jQuery( function ( $ ) {
 			} );
 		},
 		addsp: function ( id_kho, products ) {
-			console.log( products );
 			if ( products.length !== 0 ) {
 				$.post( ProductParams.ajaxUrl, {
 					action: 'them_product_kho',
@@ -234,6 +234,7 @@ jQuery( function ( $ ) {
 					let data_sp_kho = {
 						id: response.data,
 						products: products,
+						id_kho: id_kho,
 					};
 					$( '.modal-body__product' ).html( response.data );
 					$( '.modal-body__content[data-kho=' + id_kho + ']' ).append( product_kho.htmlLayout( data_sp_kho ) );
@@ -249,7 +250,7 @@ jQuery( function ( $ ) {
 				var products = [];
 				$( this ).closest( '.modal-body' ).find( ".add-product" ).each( function ( index ) {
 					var id_product = $( this ).find( 'select[name="product_name"] option:selected' ).val();
-					console.log( 'id_product', id_product );
+					//console.log( 'id_product', id_product );
 					var name_sp = $( this ).find( 'select[name="product_name"] option:selected' ).text();
 					var number = $( this ).find( '#number_product' ).val();
 					if ( id_product !== '' ) {
@@ -308,10 +309,12 @@ jQuery( function ( $ ) {
 			} );
 		},
 		removeButton: function () {
-			$( '.modal-body .remove-product' ).on( 'click', function ( e ) {
-				var id_kho = $( this ).closest( '.modal' ).attr( 'data-modal' );
+			$( '.remove-product' ).on( 'click', function ( e ) {
+				var id_kho = $( this ).attr( 'data-kho' );
+				console.log( 'id_kho', id_kho );
 				let parent = $( this ).parents( '.modal-body__inner' ),
 					id_product = parent.data( 'product' );
+				console.log( 'id_product', id_product );
 				$( '.confirm-remove' ).addClass( 'confirmed' );
 				$( '.confirm-remove' ).attr( 'data-id', id_product );
 				$( '.confirm-remove' ).attr( 'data-kho', id_kho );
